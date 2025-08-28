ef8036a6d571d5b1517311e3f658936477d6b459
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-08-28 11:09:04 -0400

    api: add concise alias names (smooth_*, bilat_*, guided_*, tv_*, mppca_*, vst_*, suggest_params); update vignette; regenerate docs

---
+++ R/zzz-aliases.R
@@ -0,0 +1,106 @@
+#' Preferred alias for fmrismooth_default
+#'
+#' See [fmrismooth_default()] for details.
+#' @inheritParams fmrismooth_default
+#' @seealso fmrismooth_default
+#' @export
+smooth_auto <- fmrismooth_default
+
+#' Preferred alias for fmrismooth_mppca_pipeline
+#'
+#' See [fmrismooth_mppca_pipeline()] for details.
+#' @inheritParams fmrismooth_mppca_pipeline
+#' @seealso fmrismooth_mppca_pipeline
+#' @export
+smooth_mppca <- fmrismooth_mppca_pipeline
+
+#' Preferred alias for fast_bilateral_lattice3d
+#' @inheritParams fast_bilateral_lattice3d
+#' @seealso fast_bilateral_lattice3d
+#' @export
+bilat_lattice3d <- fast_bilateral_lattice3d
+
+#' Preferred alias for fast_bilateral_lattice4d
+#' @inheritParams fast_bilateral_lattice4d
+#' @seealso fast_bilateral_lattice4d
+#' @export
+bilat_lattice4d <- fast_bilateral_lattice4d
+
+#' Preferred alias for fast_bilateral_joint3d
+#' @inheritParams fast_bilateral_joint3d
+#' @seealso fast_bilateral_joint3d
+#' @export
+bilat_joint3d <- fast_bilateral_joint3d
+
+#' Preferred alias for fast_bilateral_joint4d
+#' @inheritParams fast_bilateral_joint4d
+#' @seealso fast_bilateral_joint4d
+#' @export
+bilat_joint4d <- fast_bilateral_joint4d
+
+#' Preferred alias for st_guided_filter3d
+#' @inheritParams st_guided_filter3d
+#' @seealso st_guided_filter3d
+#' @export
+guided_filter3d <- st_guided_filter3d
+
+#' Preferred alias for st_guided_filter4d
+#' @inheritParams st_guided_filter4d
+#' @seealso st_guided_filter4d
+#' @export
+guided_filter4d <- st_guided_filter4d
+
+#' Preferred alias for stv_denoise4d
+#' @inheritParams stv_denoise4d
+#' @seealso stv_denoise4d
+#' @export
+tv_denoise4d <- stv_denoise4d
+
+#' Preferred alias for stv_robust4d
+#' @inheritParams stv_robust4d
+#' @seealso stv_robust4d
+#' @export
+tv_robust4d <- stv_robust4d
+
+#' Alternate short alias for mp_pca4d
+#' @inheritParams mp_pca4d
+#' @seealso mp_pca4d
+#' @export
+mppca4d <- mp_pca4d
+
+#' Preferred alias for mp_pca4d
+#' @inheritParams mp_pca4d
+#' @seealso mp_pca4d
+#' @export
+mppca_denoise4d <- mp_pca4d
+
+#' Preferred alias for estimate_sigma_rician
+#' @inheritParams estimate_sigma_rician
+#' @seealso estimate_sigma_rician
+#' @export
+estimate_rician_sigma <- estimate_sigma_rician
+
+#' Preferred alias for vst_forward_rician
+#' @inheritParams vst_forward_rician
+#' @seealso vst_forward_rician
+#' @export
+vst_forward <- vst_forward_rician
+
+#' Preferred alias for vst_inverse_rician
+#' @inheritParams vst_inverse_rician
+#' @seealso vst_inverse_rician
+#' @export
+vst_inverse <- vst_inverse_rician
+
+#' Preferred alias for vst_wrap
+#' @inheritParams vst_wrap
+#' @seealso vst_wrap
+#' @export
+vst_denoise <- vst_wrap
+
+#' Preferred alias for recommend_params
+#' @inheritParams recommend_params
+#' @seealso recommend_params
+#' @export
+suggest_params <- recommend_params
+
